3253ccd3ceb5a074fe4b4708ca60a59b651d3b49
Tucker <tucker.bichsel@gmail.com>
2022-09-21 22:53:32 -0500

    add docker image env variables to set API info - Tucker

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const env = process.env;
 
 const config = {
   api: { 
-    host: env.API_HOST || 'localhost:3001',
-    protocol: env.API_PROTOCOL || 'http',
+    host: env.REACT_APP_API_HOST || 'localhost:3001',
+    protocol: env.REACT_APP_API_PROTOCOL || 'http',
   },
 };
 
