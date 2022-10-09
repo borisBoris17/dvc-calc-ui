f91597166af344393f2f98283fcb1924e7c51f02
Tucker <tuckerbichsel@tuckers-mbp.lan>
2022-10-08 20:52:28 -0500

    Create side navigation menu - tucker

--- src/App.js
+++ src/App.js
@@ -13,10 +13,52 @@ import ImportRoomTypeComponent from './Components/ImportRoomTypeComponent';
 import ImportViewTypeComponent from './Components/ImportViewTypeComponent';
 import ImportFromFileComponent from './Components/ImportFromFileComponent';
 import ImportPointBlockComponent from './Components/ImportPointBlockComponent';
+import { AppBar, Toolbar, IconButton, Typography, Button, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Drawer } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import BedIcon from '@mui/icons-material/Bed';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import AddchartIcon from '@mui/icons-material/Addchart';
+
 const config = require('./config');
+
+const links = [
+  {
+    name: "Home",
+    location: "/",
+  },
+  {
+    name: "Import Room",
+    location: "/importRoomType"
+  },
+  {
+    name: "Import View",
+    location: "/importViewType"
+  },
+  {
+    name: "Import Points",
+    location: "/importPointsTable"
+  },
+  {
+    name: "Import Point Block",
+    location: "/importPointBlock"
+  },
+]
 
 function App() {
   const [resorts, setResorts] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     axios.get(`${config.api.protocol}://${config.api.host}/dvc-calc-api/resorts`).then(resp => {
@@ -27,14 +69,84 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <AppBar position='static'>
+
+
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={handleMenuClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              id="sideMenu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <List>
+                  <ListItem component={Link} href="/">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <CalculateIcon></CalculateIcon>
+                      </ListItemIcon>
+                      <ListItemText primary="DVC Calculator" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem component={Link} href="/importRoomType">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <BedIcon></BedIcon>
+                      </ListItemIcon>
+                      <ListItemText primary="Import Room Type" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem component={Link} href="/importViewType">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <VisibilityIcon></VisibilityIcon>
+                      </ListItemIcon>
+                      <ListItemText primary="Import View Type" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem component={Link} href="/importPointBlock">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <DateRangeIcon></DateRangeIcon>
+                      </ListItemIcon>
+                      <ListItemText primary="Import Point Block" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem component={Link} href="/importPointsTable">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <AddchartIcon></AddchartIcon>
+                      </ListItemIcon>
+                      <ListItemText primary="Import Point Table" />
+                    </ListItemButton>
+                  </ListItem>
+              </List>
+            </Drawer>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+
+        </AppBar>
         <Routes>
-          <Route path="/" element={<DVCCalculatorComponent resorts={resorts}/> } />
-          <Route path="/importPoints" element={<ImportPointsComponent resorts={resorts}/> } />
-          <Route path="/importPointsTable" element={<ImportPointsTableComponent resorts={resorts}/> } />
-          <Route path="/importPointBlock" element={<ImportPointBlockComponent resorts={resorts} /> } />
-          <Route path="/importRoomType" element={<ImportRoomTypeComponent resorts={resorts}/> } />
-          <Route path="/importViewType" element={<ImportViewTypeComponent resorts={resorts}/> } />
-          <Route path="/importFromFile" element={<ImportFromFileComponent />} /> 
+          <Route path="/" element={<DVCCalculatorComponent resorts={resorts} />} />
+          <Route path="/importPoints" element={<ImportPointsComponent resorts={resorts} />} />
+          <Route path="/importPointsTable" element={<ImportPointsTableComponent resorts={resorts} />} />
+          <Route path="/importPointBlock" element={<ImportPointBlockComponent resorts={resorts} />} />
+          <Route path="/importRoomType" element={<ImportRoomTypeComponent resorts={resorts} />} />
+          <Route path="/importViewType" element={<ImportViewTypeComponent resorts={resorts} />} />
+          <Route path="/importFromFile" element={<ImportFromFileComponent />} />
         </Routes>
       </div>
     </Router>
